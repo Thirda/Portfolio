6ac5a4a328011f7ae30fadde9a65bff7beb31789
Therda <antonestival@gmail.com>
2021-09-24 14:53:38 +0200

    add v3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Presentation from "./components/Home/presentation/presentation";
 import Parcours from './components/Home/parcours/parcours';
 import Skills from './components/Home/skills/skills.jsx';
 import Portfolio from './components/Home/portfolio/portfolio';
-import Contact from './components/Home/contact/contact';
+// import Contact from './components/Home/contact/contact';
 
 import './App.css';
 
